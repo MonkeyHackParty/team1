1daa0627bf1f13c62e5d3185d65795319cfbe567
mutyuki <belandinging01@gmail.com>
2024-09-11 02:51:50 +0900

    tetrisの基盤作成

---
+++ tetris/Assets/Scripts/Rounding.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public static class Rounding
+{
+    //数値の四捨五入
+    public static Vector2 Round(Vector2 i)
+    {
+        return new Vector2(Mathf.Round(i.x), Mathf.Round(i.y));
+    }
+    public static Vector3 Round(Vector3 i)
+    {
+        return new Vector3(Mathf.Round(i.x), Mathf.Round(i.y));
+    }
+}
